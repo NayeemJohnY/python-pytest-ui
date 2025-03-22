d5f2c0da477ea41af90f25deabb45b97de9363cf
NayeemJohn <44828643+NayeemJohnY@users.noreply.github.com>
2025-03-22 17:20:09 +0530

    Update to headless

--- test_login.py
+++ test_login.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.by import By
 
 
 def test_valid_login():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(options)
     driver.get('https://www.saucedemo.com/')
     driver.find_element(By.ID, 'user-name').send_keys('standard_user')
     driver.find_element(By.ID, 'password').send_keys('secret_sauce')
